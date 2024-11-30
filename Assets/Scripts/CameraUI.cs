c0dfc695f334b7afc41b22586005372afb3d216b
mjyc <62436931+GoshawkGunyou@users.noreply.github.com>
2024-11-30 19:10:20 +0800

    init

---
+++ Assets/Scripts/CameraUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraUI : MonoBehaviour
+{
+
+    public RectTransform uiElement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiElement.anchorMin = new Vector2(1, 1);
+        uiElement.anchorMax = new Vector2(1, 1);
+        uiElement.pivot = new Vector2(1, 1);
+        uiElement.anchoredPosition = new Vector2(-20, -20);
+    }
+
+}
